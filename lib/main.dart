07e1b8ed80500754c2bfb28946d521d6377241f2
WilmaAhlenLundqvist <guswilah@student.gu.se>
2025-09-10 18:52:50 +0200

    Ändrat till custom theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,18 @@ main() { //startpunkt
 class MyApp extends StatelessWidget { //huvudwidget
   MyApp({super.key});
 
+  final ThemeData myCustomTheme = ThemeData(
+    primaryColor: Colors.purple[900],
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.purple,
+      brightness: Brightness.light,),
+    ); 
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "TIG333 TODO",
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
+      theme: myCustomTheme,
       home: TodoListPage(),
       );
   }
