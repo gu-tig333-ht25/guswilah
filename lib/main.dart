0251e5d8b3e2ef4828fc5f850ec35c95753765f3
WilmaAhlenLundqvist <guswilah@student.gu.se>
2025-09-11 13:46:43 +0200

    gjort listan
    det är nått knas här i slutet dock

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ main() { //startpunkt
 class MyApp extends StatelessWidget { //huvudwidget
   MyApp({super.key});
 
-  final ThemeData myCustomTheme = ThemeData(
+  final ThemeData myCustomTheme = ThemeData( //bestäm tema och färg
     primaryColor: Colors.purple[900],
     colorScheme: ColorScheme.fromSeed(
       seedColor: Colors.purple,
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget { //huvudwidget
     ); 
  
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { //bestäm titel och vad som öppnas först
     return MaterialApp(
       title: "TIG333 TODO",
       theme: myCustomTheme,
@@ -23,3 +23,49 @@ class MyApp extends StatelessWidget { //huvudwidget
       );
   }
 }
+
+class TodoListPage extends StatelessWidget {
+  TodoListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {//gör listan
+    final todos = [
+      "Write a book",
+      "Do homework",
+      "Tidy room",
+      "Watch TV",
+      "Nap",
+      "Shop groceries",
+      "Have fun",
+      "Meditate",
+    ];
+
+    return Scaffold(
+      appBar: Appbar(
+        title: Text("TIG333 TODO"),
+        backgroundColor: Colors.grey[300],
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: (){
+              showDialog(
+                context: context,
+                builder: (context){
+                  return AlertDialog(
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text("all"),
+                        Text("done"),
+                        Text("undone"),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+  } //här är det helt klart nått knas
+  }
