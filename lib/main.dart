b5b78957a46ef10c8e8ce1da71ad0b52cd2c8437
WilmaAhlenLundqvist <guswilah@student.gu.se>
2025-09-11 15:43:05 +0200

    Gjort sida 2

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class TodoListPage extends StatelessWidget {
     ];
 
     return Scaffold(
-      appBar: Appbar(
+      appBar: AppBar(
         title: Text("TIG333 TODO"),
         backgroundColor: Colors.grey[300],
         actions: [ //knappar
@@ -89,7 +89,7 @@ class TodoListPage extends StatelessWidget {
             floatingActionButton: FloatingActionButton( //plustecknet på sidan
               onPressed:(){
                 Navigator.push(context,
-                MaterialPageRoute(builder:(context)=> const //öppna nästa sida
+                MaterialPageRoute(builder:(context)=> //öppna nästa sida
                 AddTodoPage()), //måste göra klassen
                 );
               },
@@ -98,3 +98,44 @@ class TodoListPage extends StatelessWidget {
           );
         }
       }
+
+class AddTodoPage extends StatelessWidget {
+  AddTodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TIG333 TODO"),
+        backgroundColor: Colors.grey[300],
+        leading:IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+          ),
+        ),
+
+  body: Padding(
+    padding: EdgeInsets.all(20.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: "What are you going to do?",
+          ),
+        ),
+        SizedBox(height:20),
+        Center(
+          child: TextButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+            label: Text("ADD"),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+}
