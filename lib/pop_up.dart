6b48ee64acb5b7238c1467ef115b018df25c3eae
WilmaAhlenLundqvist <guswilah@student.gu.se>
2025-09-19 16:50:55 +0200

    Kommentarer för pop_up

--- lib/pop_up.dart
+++ lib/pop_up.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'main.dart'; //för filtret
 
-class FilterPopUp extends StatelessWidget {
-  const FilterPopUp({super.key});
+class FilterPopUp extends StatelessWidget { //stateless för popupen aldrig ändras
+  const FilterPopUp({super.key}); //const = oföränderlig konstruktor
 
   @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
+  Widget build(BuildContext context) { //bygger UI:t, context för att veta vart i trädet vi är
+    return AlertDialog( //inbyggd popupwidget
       title: const Text("Filter todos"),
-      content: Column(
+      content: Column( //visar alternativen i popupen
         mainAxisSize: MainAxisSize.min,
               children: [
           ListTile(
-            title: const Text("All"),
-            onTap: () => Navigator.pop(context, FilterOption.all),
+            title: const Text("All"), //texten som står
+            onTap: () => Navigator.pop(context, FilterOption.all), //vid tryck stäng popup och returnera värdet till TodoListPage
           ),
           ListTile(
             title: const Text("Done"),
